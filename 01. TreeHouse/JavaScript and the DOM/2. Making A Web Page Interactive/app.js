80a6d81eab69818e4de32fa5d581469ae2945b66
Bappy Golder <bups.co@gmail.com>
2016-12-15 13:08:23 +1100

    querySelector, querySelectorAll :8ball:
    Also working with :nth-child attribute

--- 01. TreeHouse/JavaScript and the DOM/2. Making A Web Page Interactive/app.js
+++ 01. TreeHouse/JavaScript and the DOM/2. Making A Web Page Interactive/app.js
@@ -9,8 +9,25 @@ for(let i = 0; i < myList.length; i++){
 }
 
 //show error for non purple items
-const errorNotPurple = document.getElementsByClassName('error-not-purple');
+//const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+//alternative way to select
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
 
 for(let i = 0; i < errorNotPurple.length; i++){
 	errorNotPurple[i].style.color = 'red';
+}
+
+//using nth child
+const evens = document.querySelectorAll('li:nth-child(even)');
+
+for(let i = 0; i < evens.length; i++){
+	evens[i].style.backgroundColor = 'rgb(220, 210, 230)';
+}
+
+//using nth child 2
+const odds = document.querySelectorAll('li:nth-child(odd)');
+
+for(let i = 0; i < odds.length; i++){
+	odds[i].style.backgroundColor = '#eeeeee';
 }
