3608ccc0283bc134f898b174b975e49017a167d9
Bappy Golder <bups.co@gmail.com>
2016-12-15 12:42:30 +1100

    tag and class selectors

--- 01. TreeHouse/JavaScript and the DOM/2. Making A Web Page Interactive/app.js
+++ 01. TreeHouse/JavaScript and the DOM/2. Making A Web Page Interactive/app.js
@@ -1,7 +1,16 @@
 /*jshint esversion: 6 */
 
+
+//change all list elements to purple
 const myList = document.getElementsByTagName('li');
 
 for(let i = 0; i < myList.length; i++){
 	myList[i].style.color = 'purple';
+}
+
+//show error for non purple items
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for(let i = 0; i < errorNotPurple.length; i++){
+	errorNotPurple[i].style.color = 'red';
 }
