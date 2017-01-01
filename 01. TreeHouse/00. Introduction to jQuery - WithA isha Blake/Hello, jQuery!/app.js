284bfe6c7c6261fe59d78b12211ab9c31d003ba3
Bappy Golder <bups.co@gmail.com>
2017-01-01 21:23:27 +1100

    selector practice
    practicing with chaning selectors and using variations of selectors

--- 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/Hello, jQuery!/app.js
+++ 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/Hello, jQuery!/app.js
@@ -1,3 +1,21 @@
+/*jshint esversion: 6*/
+
 $('li').on('click', function() {
     $(this).text("Clicked!").delay(2000).fadeOut(400);
 });
+
+
+/*selector examples
+	element:
+		$(p), $(a), $(li) etc.
+	id & class selectors:
+		$('.blog-post');
+		$('#about');
+	attribute selectors:
+		$("[name='newletter']");
+
+note: they are same as class*/
+
+//make all the odd elements in the second list background blue
+const list2 = $('.ul2 li');
+list2.not(":even").css('background-color', 'steelblue').css('color', '#e2e2e2');
