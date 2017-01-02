15b58c554cb15f202132ddf3d1f30cd06a5e4780
Bappy Golder <bups.co@gmail.com>
2017-01-02 14:30:29 +1100

    explanation of a call back function

--- 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/Events/app.js
+++ 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/Events/app.js
@@ -5,3 +5,10 @@
 $('body').click((event) => {
     console.log(event.target);
 });
+
+//how I understand call back functions
+/*call back functions are functions that are called/executed when something else happens (or some event/s takes place). The primary difference between a call back function and a normal function:
+- normal functions are first declared and then called in the JavaScript code later on.
+- a call back function is never called/written to execute later in the code. Rather they excuted based on an event that happens later. Due to this a call back function often have the following two parameters
+  1. At what event the call back function will be run (or the event)
+  2. What will be done or the function that will be run*/
