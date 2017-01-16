1ce50214296a52dd8d2b362f8ec8358ec42300fa
Bappy Golder <bups.co@gmail.com>
2017-01-16 23:19:54 +1100

    changes

---
+++ 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/DOM Traversal/app.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6*/
+
+var students = [{
+    name: "Ben"
+}, {
+    name: "Scott"
+}, {
+    name: "Evs"
+}, {
+    name: "Grace"
+}, {
+    name: "Puneet"
+}, {
+    name: "Hurshel"
+}, {
+    name: "Adam"
+}, {
+    name: "Steve"
+}, {
+    name: "Pat"
+}];
+
+//show the data through for each loop
+students.forEach(function(elem) {
+    console.log(elem.name);
+});
+
+// console.log('Ben');
+// console.log('Scott');
+// console.log('Evs');
