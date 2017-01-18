fee05393d421f82516d87f8a381fb91b56d20f95
Bappy Golder <bups.co@gmail.com>
2017-01-19 10:25:58 +1100

    empty form field after entering the values

--- 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/website interaction with jQuery - Pet Adoption Site/app.js
+++ 01. TreeHouse/00. Introduction to jQuery - WithA isha Blake/website interaction with jQuery - Pet Adoption Site/app.js
@@ -1,6 +1,8 @@
 /*jshint esversion: 6*/
 //local project location: file:///C:/Users/Bappy%20Golder/Desktop/Projects/Learning/01.%20TreeHouse/00.%20Introduction%20to%20jQuery%20-%20WithA%20isha%20Blake/website%20interaction%20with%20jQuery%20-%20Pet%20Adoption%20Site/index.html
 
+
+//change text on mouse hover and restore tex on when mouse is moved away
 $('.loc').hover( // on mouse hover
     function() {
         //console.log('mouse enter');
@@ -14,22 +16,28 @@ $('.loc').hover( // on mouse hover
 //make changes on ADD button click
 $('#add-pet').on('click', function() {
     //collect all the values from the input fields
-    var $petName = $('#pet-name').val();
-    var $petSpecies = $('#pet-species').val();
-    var $petNotes = $('#pet-notes').val();
+    var $petName = $('#pet-name');
+    var $petSpecies = $('#pet-species');
+    var $petNotes = $('#pet-notes');
 
+    //testing: show value
     console.log($petName);
     console.log($petSpecies);
     console.log($petNotes);
 
     //format the information in the fields in correct HTML
-    var $newPet = '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $petName +
-        '</p><p><strong>Species:</strong> ' + $petSpecies +
-        '</p><p><strong>Notes:</strong> ' + $petNotes +
+    var $newPet = '<section class="six columns"><div class="card"><p><strong>Name:</strong> ' + $petName.val() +
+        '</p><p><strong>Species:</strong> ' + $petSpecies.val() +
+        '</p><p><strong>Notes:</strong> ' + $petNotes.val() +
         '</p><span class="close">&times;</span></div></section>';
 
     //add the HTML on the page
     $('#posted-pets').append($newPet);
+
+    //empty form fields after button click
+    $('#pet-name').val('');
+    $('#pet-species').val('Dog');
+    $('#pet-notes').val('');
 });
 
 //puppy image faddeIn
